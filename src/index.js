de7745cac88c4e15cac67325e2d085a01d77304f
Leo <leomutungi19@gmail.com>
2022-08-28 12:11:43 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -13,3 +13,32 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
+  const addBtn = document.querySelector("#new-toy-btn");
+  const toyForm = document.querySelector(".container");
+  let divCollect = document.querySelector("#toy-collection");
+
+    function getToys() {
+      return fetch("http://localhost:3000/toys").then((res) => res.json());
+    }
+
+    function postToy(toy_data) {
+      fetch("http://localhost:3000/toys", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          name: toy_data.name.value,
+          image: toy_data.image.value,
+          likes: 0,
+        }),
+      })
+        .then((res) => res.json())
+        .then((obj_toy) => {
+          let new_toy = renderToys(obj_toy);
+          divCollect.append(new_toy);
+        });
+    }
